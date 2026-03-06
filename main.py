91533dfb83abec64f20408e1a482d73f5807a61f
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-03-06 23:14:23 +0300

    add: calculate

--- main.py
+++ main.py
@@ -14,3 +14,25 @@ def divide(a,b):
 
 def power(a,b):
     return a ** b
+
+a = float(input("Enter your first number:"))
+b = float(input("Enter your second number:"))
+operation = input("Enter operation (+ , - , * , / , **):")
+
+def calculate():
+    a = float(input("Enter your first number:"))
+    b = float(input("Enter your second number:"))
+    operation = input("Enter operation (+ , - , * , / , **):")
+
+    if operation == "+":
+        print(add(a,b))
+    elif operation == "-":
+        print(subtract(a,b))
+    elif operation == "*":
+        print(multiply(a,b))
+    elif operation == "/":
+        print(divide(a,b))
+    elif operation == "**":
+        print(power(a,b))
+    else :
+        print("Invaild operation")
